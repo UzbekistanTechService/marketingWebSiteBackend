de0888ee0a66145fd987bbc8b79a88c1268198a2
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-11-20 16:09:31 +0500

    poll rest api add

---
+++ src/poll/dto/poll.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+import { PollServiceType } from "../models/poll.model";
+
+export class CreatePollDto {
+  @ApiProperty({
+    type: 'string',
+    example: '998901234567',
+  })
+  @IsNotEmpty()
+  @IsString()
+  phone_number: string;
+
+  @ApiProperty({
+    type: 'string',
+    example: 'SMM Marketing Services',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(50)
+  name: string;
+
+  @ApiProperty({
+    type: 'enum',
+    enum: PollServiceType,
+    example:'smm',
+  })
+  @IsNotEmpty()
+  @IsEnum(PollServiceType)
+  service: PollServiceType;
+}
