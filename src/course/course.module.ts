04d1f93d3d707846532c2a440794bc7cc14b1661
Dilshod1421 <dilshod7861@gmail.com>
2023-11-21 15:33:03 +0500

    course

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Course } from './models/course.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Course])],
   controllers: [CourseController],
   providers: [CourseService],
 })
-export class CourseModule {}
+export class CourseModule { }
--- src/course/models/course.model.ts
+++ src/course/models/course.model.ts
@@ -1,1 +1,34 @@
-export class Course {}
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+interface CourseAttributes {
+    name: string;
+    price: number;
+    description: string;
+}
+
+@Table({ tableName: 'course' })
+export class Course extends Model<Course, CourseAttributes> {
+    @Column({
+        type: DataType.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    })
+    id: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    name: string;
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    price: number;
+
+    @Column({
+        type: DataType.TEXT,
+    })
+    description: string;
+}
