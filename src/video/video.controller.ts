7e70a35e1f0f2f963518cca08c90c89322a8f19e
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-11-27 13:46:18 +0500

    video steraming

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Headers,
+  Param,
+  ParseIntPipe,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { VideoService } from './video.service';
 @Controller('video')
 export class VideoController {
   constructor(private readonly videoService: VideoService) {}
+
+  @Get('stream/:id')
+  @Header('Accept-Ranges', 'bytes')
+  @Header('Content-Type', 'video/mp4')
+  async getStreamVideo(
+    @Param('id', ParseIntPipe) id: number,
+    @Headers() headers: Headers,
+    @Res() res: Response,
+  ) {
+    return this.videoService.getStreamVideo(res, headers, id);
+  }
 }
--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
+import { createReadStream, statSync } from 'fs';
+import { Response } from 'express';
+import { Video } from './models/video.model';
+import { InjectModel } from '@nestjs/sequelize';
+import { join } from 'path';
 @Injectable()
-export class VideoService {}
+export class VideoService {
+  constructor(@InjectModel(Video) private videoRepository: typeof Video) {}
+  async getStreamVideo(res: Response, headers, id: number) {
+    try {
+      const existVideo = await this.videoRepository.findOne({ where: { id } });
+      if (!existVideo) {
+        return {
+          message: 'Video not found',
+        };
+      }
+      const videoPath = join(__dirname, '..', 'static', existVideo.file_name);
+      const { size } = statSync(videoPath);
+      const videoRange = headers.range;
+      if (videoRange) {
+        console.log(videoRange);
+        const parts = videoRange.replace(/bytes=/, '').split('-');
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
+        const chunkSize = end - start + 1;
+        const readStreamfile = createReadStream(videoPath, {
+          start,
+          end,
+          highWaterMark: 60,
+        });
+        const head = {
+          'Content-Range': `bytes ${start}-${end}/${size}`,
+          'Content-Length': chunkSize,
+        };
+
+        res.writeHead(HttpStatus.PARTIAL_CONTENT, head); //206
+        readStreamfile.pipe(res);
+      } else {
+        const head = {
+          'Content-Length': size,
+        };
+
+        res.writeHead(HttpStatus.OK, head); //200
+        createReadStream(videoPath).pipe(res);
+      }
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+}
