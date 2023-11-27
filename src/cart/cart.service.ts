296ea300d9929f0a85aac2198667dadcce7c679a
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2023-11-27 16:42:01 +0500

    cart crud added

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Cart } from './models/cart.model';
+import { UserService } from 'src/user/user.service';
+import { CourseService } from 'src/course/course.service';
+import { CreateCartDto } from './dto/cart.dto';
+import { Op } from 'sequelize';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(
+    @InjectModel(Cart) private cartRepository: typeof Cart,
+    private readonly userService: UserService,
+    private readonly courseService: CourseService,
+  ) {}
+  async create(cartDto: CreateCartDto) {
+    try {
+      const { user_id, course_id } = cartDto;
+      await this.getOne(`${user_id}-${course_id}`);
+      await this.userService.getByID(user_id);
+      await this.courseService.getByID(course_id);
+
+      const cart = await this.cartRepository.create(cartDto);
+      return cart;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async getAll() {
+    const carts = await this.cartRepository.findAll();
+    return carts;
+  }
+
+  async getByID(id: number) {
+    try {
+      const cart = await this.cartRepository.findOne({ where: { id } });
+
+      if (!cart) {
+        return { message: 'Cart not found' };
+      }
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async delete(id: number) {
+    try {
+      await this.getByID(id);
+      await this.cartRepository.destroy({ where: { id } });
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async getOne(ids: string) {
+    try {
+      const user_id = Number(ids.split('-'))[0];
+      const course_id = Number(ids.split('-'))[1];
+      const cart = await this.cartRepository.findOne({
+        where: {
+          [Op.and]: [{ user_id }, { course_id }],
+        },
+        include: { all: true },
+      });
+      return cart;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+}
--- src/cart/dto/cart.dto.ts
+++ src/cart/dto/cart.dto.ts
@@ -1,1 +1,23 @@
-export class CartDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumber,
+} from 'class-validator';
+
+export class CreateCartDto {
+  @ApiProperty({
+    type: 'number',
+    example: '12',
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  user_id: number;
+
+  @ApiProperty({
+    type: 'number',
+    example: '12',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  course_id: number;
+}
